299237d6f68815c979fafbe854c93e30d9c70258
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-04-20 09:36:14 -0300

    feat: configuration routes adapter

---
+++ src/main/adapters/server-routes-adapter.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { Controller } from "../protocols/controller";
+import { HttpRequest } from "../protocols/http";
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      headers: req.headers,
+      params: req.params,
+      query: req.query,
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+
+    res.status(httpResponse.statusCode);
+
+    if (httpResponse.statusCode === 500) {
+      res.send({ error: "SERVER ERROR: " + httpResponse.body.message });
+    } else if (
+      httpResponse.statusCode >= 400 &&
+      httpResponse.statusCode < 500
+    ) {
+      res.send({ error: httpResponse.body.message });
+    } else {
+      res.send(httpResponse.body);
+    }
+  };
+};
