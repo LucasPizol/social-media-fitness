1017777387551b3507683b37a638e5c82f8732ff
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-04-20 23:51:55 -0300

    test: load exercise tests

---
+++ src/modules/exercise/use-case/load-exercise/load-exercise.spec.ts
@@ -0,0 +1,44 @@
+import { MockProxy, mock } from "jest-mock-extended";
+import { LoadExerciseUseCase } from "./load-exercise";
+import {
+  LoadExercise,
+  LoadExerciseRepository,
+} from "./load-exercise-protocols";
+
+describe("Load Exercise Use Case", () => {
+  let sut: LoadExercise;
+  let loadExerciseRepository: MockProxy<LoadExerciseRepository>;
+
+  beforeEach(() => {
+    loadExerciseRepository = mock();
+
+    sut = new LoadExerciseUseCase(loadExerciseRepository);
+  });
+
+  const fakeDate = new Date();
+
+  it("should return exercise on success", async () => {
+    loadExerciseRepository.load.mockResolvedValue([
+      {
+        id: "any_id",
+        name: "any_name",
+        userId: "any_user_id",
+        createdAt: fakeDate,
+        updatedAt: fakeDate,
+      },
+    ]);
+
+    const response = await sut.load("any_user_id");
+
+    expect(loadExerciseRepository.load).toHaveBeenCalledTimes(1);
+    expect(response).toEqual([
+      {
+        id: "any_id",
+        name: "any_name",
+        userId: "any_user_id",
+        createdAt: fakeDate,
+        updatedAt: fakeDate,
+      },
+    ]);
+  });
+});
